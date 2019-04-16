d25fc89dd4189ada55e53fce729d8150167c9808
Rasmus Praestholm <cervator@gmail.com>
2019-04-16 14:13:44 -0500

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example - now with CHANGES!"
 
 	from := ""
 	if r.URL != nil {
